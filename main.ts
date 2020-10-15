a291443c96631eabfe106549197120403c3a4e97
profdetech <emmanuel.mouronval@gmail.com>
2020-10-15 20:50:00 +0200

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,17 +1,21 @@
+input.onButtonPressed(Button.A, function () {
+    repere += 1
+})
 serial.redirect(
 SerialPin.P14,
 SerialPin.P0,
 BaudRate.BaudRate4800
 )
+let repere = 0
 while (!(input.buttonIsPressed(Button.A))) {
     basic.pause(100)
 }
-let entete = "temps" + ";" + "analogique"
+let entete = "temps" + ";" + "analogique" + ";" + "repere"
 serial.writeLine(entete)
 basic.showLeds(`
     . . # . .
     . . # . .
-    . . # . #
+    # . # . #
     . # # # .
     . . # . .
     `)
@@ -24,7 +28,7 @@ basic.showLeds(`
     . . . . .
     `)
 basic.forever(function () {
-    entete = "" + input.runningTime() + ";" + pins.analogReadPin(AnalogPin.P1)
+    entete = "" + input.runningTime() + ";" + pins.analogReadPin(AnalogPin.P1) + ";" + repere
     serial.writeLine(entete)
     basic.pause(100)
 })
