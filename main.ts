39a48cd59673598eb9eeb0555b0c92080882e066
profdetech <emmanuel.mouronval@gmail.com>
2020-10-15 21:07:46 +0200

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,13 +1,15 @@
 input.onButtonPressed(Button.B, function () {
     repere += 1
+    basic.showNumber(repere)
 })
 let temps = 0
+let repere = 0
 serial.redirect(
 SerialPin.P14,
 SerialPin.P0,
 BaudRate.BaudRate4800
 )
-let repere = 0
+repere = 0
 while (!(input.buttonIsPressed(Button.A))) {
     basic.pause(100)
 }
@@ -21,14 +23,8 @@ basic.showLeds(`
     . . # . .
     `)
 basic.pause(500)
-basic.showLeds(`
-    . . . . .
-    . . . . .
-    . . . . .
-    . . . . .
-    . . . . .
-    `)
 let init = input.runningTime()
+basic.clearScreen()
 basic.forever(function () {
     temps = input.runningTime() - init
     entete = "" + temps + ";" + pins.analogReadPin(AnalogPin.P1) + ";" + repere
